e3a54b62012ca7b00a2a8f02321a0631a9c5b2db
negimohit1209 <mohitsngh1997@gmail.com>
2018-04-04 23:36:47 +0530

    version 6 final commit add auth

---
+++ models/campground.js
@@ -0,0 +1,29 @@
+// var mongoose = require("mongoose");
+
+
+// var campgroundSchema = new mongoose.Schema({
+//     name: String,
+//     image: String,
+//     description: String
+// });
+
+
+// var Campground = mongoose.model("Campground", campgroundSchema);
+// module.exports = Campground;
+
+//copied
+var mongoose = require("mongoose");
+ 
+var campgroundSchema = new mongoose.Schema({
+   name: String,
+   image: String,
+   description: String,
+   comments: [
+      {
+         type: mongoose.Schema.Types.ObjectId,
+         ref: "Comment"
+      }
+   ]
+});
+ 
+module.exports = mongoose.model("Campground", campgroundSchema);
