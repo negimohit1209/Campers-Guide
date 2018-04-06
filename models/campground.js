685d59e515fd26f90b318e12c0862b113158b2c2
negimohit1209 <mohitsngh1997@gmail.com>
2018-04-06 22:49:22 +0530

    refactored landing page

--- models/campground.js
+++ models/campground.js
@@ -18,6 +18,7 @@ var campgroundSchema = new mongoose.Schema({
    name: String,
    image: String,
    description: String,
+   price: String,
    author: {
    	id: {
    		type: mongoose.Schema.Types.ObjectId,
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -30,11 +30,12 @@ router.post("/", middleware.isLoggedIn, function(req, res){
     var name = req.body.name;
     var image = req.body.image;
     var desc = req.body.description;
+    var price = req.body.price;
     var author = {
         id: req.user._id,
         username: req.user.username
     }
-    var newCampground = {name: name, image: image , description: desc ,author: author};
+    var newCampground = {name: name,price: price, image: image , description: desc ,author: author};
     //create new campground and save to database
     Campground.create(newCampground, function(err, newlycreated){
         if(err){
